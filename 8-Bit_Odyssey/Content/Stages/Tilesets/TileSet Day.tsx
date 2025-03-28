2b6c674840e2fe24b9c938cebc58515644d9a529
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2025-03-28 00:17:23 -0600

    Tiles first steps

---
+++ 8-Bit_Odyssey/Content/Stages/Tilesets/TileSet Day.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TileSet Day" tilewidth="16" tileheight="16" spacing="1" tilecount="32" columns="8">
+ <image source="TileSet Day.png" width="135" height="67"/>
+</tileset>
