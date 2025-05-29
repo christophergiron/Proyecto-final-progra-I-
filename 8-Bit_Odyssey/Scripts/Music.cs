1e501a4b61926eb760a5b98588552f577644388e
Miguel063 <malfarov1@miumg.edu.gt>
2025-05-29 01:15:05 -0600

    adicion del efecto de romper el bloque

--- 8-Bit_Odyssey/Scripts/Music.cs
+++ 8-Bit_Odyssey/Scripts/Music.cs
@@ -13,16 +13,17 @@ namespace Bit_Odyssey.Scripts
 {
     internal class Music
     {
-        private static Song runningAbout;
+        private static Song runningAbout;               //Todo esto y lo de abajo son las clases para que se ejecuten ya sea la musica o los efectos
         private static SoundEffect fxJump;
         private static SoundEffect fxSquish;
         private static Song underGround;
         public static SoundEffect fxDie;
         public static SoundEffect fxCoin;
+        public static SoundEffect fxBreak;
         private static bool esperaReset = false;
         private static float resetMusic = 0f;
         
-        public static void Load(ContentManager content)
+        public static void Load(ContentManager content) //aqui se cargan los archivos de la misma
         {
             runningAbout = content.Load<Song>("Music/Overworld");
             fxJump = content.Load<SoundEffect>("SoundFX/Jump");
@@ -30,9 +31,10 @@ namespace Bit_Odyssey.Scripts
             fxDie = content.Load<SoundEffect>("SoundFX/Die");
             underGround = content.Load<Song>("Music/Underground");
             fxCoin = content.Load<SoundEffect>("SoundFX/Coin");
+            fxBreak = content.Load<SoundEffect>("SoundFX/Break");
         }
 
-        public static void PlayMusicOverWorld()
+        public static void PlayMusicOverWorld() //es muy obvio que se reproduce cada cancion o efecto correspondiente
         {
             MediaPlayer.IsRepeating = true;
             MediaPlayer.Play(runningAbout);
@@ -62,13 +64,13 @@ namespace Bit_Odyssey.Scripts
         {
             fxCoin.Play();
         }
-        public static void ResetMusic(float deathFXDuration)
+        public static void ResetMusic(float deathFXDuration)  //esto hace que se repita la musica cuando se muere
         {
             esperaReset = true;
             resetMusic = deathFXDuration;
         }
 
-        public static void Update(GameTime gameTime)
+        public static void Update(GameTime gameTime) //hace que se repita la musica de forma normal, solo falta hacer que se reproduscan las versiones cuando queden 100 segundos, ojala se me olvide quitar esto                                                                                                                                                                            El balatreo 🃏
         {
             if (esperaReset)
             {
