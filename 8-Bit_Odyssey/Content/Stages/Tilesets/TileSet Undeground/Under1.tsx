7c43b2998f4527f8ca0a578af48ff76408ab34da
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2025-05-29 11:40:54 -0600

    Underground (Con tiles "nuevos") y demo player points
    Trabajo en proceso

---
+++ 8-Bit_Odyssey/Content/Stages/Tilesets/TileSet Undeground/Under1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Under1" tilewidth="32" tileheight="32" spacing="2" tilecount="32" columns="8">
+ <image source="Under1.png" trans="9494ff" width="270" height="134"/>
+</tileset>
---
+++ 8-Bit_Odyssey/Content/Stages/Tilesets/TileSet Undeground/Under4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Under4" tilewidth="32" tileheight="32" spacing="2" tilecount="16" columns="4">
+ <image source="Under4.png" trans="9494ff" width="134" height="134"/>
+</tileset>
